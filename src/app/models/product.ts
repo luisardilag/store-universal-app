e1b504b762e71a742d291fb9a8a96569d826e1fd
Luis Ardila <luisardilag@gmail.com>
2021-10-31 07:05:35 -0300

    update: create models data

---
+++ src/app/models/product.ts
@@ -0,0 +1,14 @@
+export interface Category {
+    id: number;
+    name: string;
+    type: string;
+}
+
+export interface Product {
+    id: number;
+    title: string;
+    price: number;
+    description: string;
+    category: Category;
+    images: string[];
+}
